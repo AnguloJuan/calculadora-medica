bd2b9f2c78cc9f2c74a58bbe90f418149baf5a53
JuanAngulo <jua4_20@alu.uabcs.mx>
2024-09-09 13:25:54 -0600

    Update: Borrador de bd - Add: evidencia table

--- wip_calculadora_medica.sql
+++ wip_calculadora_medica.sql
@@ -1,32 +1,50 @@
 # Archivo temporal en trabajo para el diseño de la bd
-#CREATE DATABASE calculadora_medica;
+DROP DATABASE calculadora_medica;
+CREATE DATABASE calculadora_medica;
 USE calculadora_medica;
 
 CREATE TABLE IF NOT EXISTS calculadora (
 	id INT NOT NULL PRIMARY KEY,
 	nombre VARCHAR(60) NOT NULL,
-    descripción VARCHAR(360),
+    descripción VARCHAR(360) NOT NULL,
     instrucciones VARCHAR(360) NOT NULL,
     formula VARCHAR(360) NOT NULL,
-    respaldo VARCHAR(150),
     fecha_creacion DATETIME NOT NULL,
     fecha_actualizacion DATETIME
-    
-    #parametros FOREIGN KEY
+);
+
+CREATE TABLE IF NOT EXISTS parametro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(32) NOT NULL,
+    tipo_campo ENUM('text', 'number', 'select', 'radio') NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS parametros (
 	id INT NOT NULL PRIMARY KEY,
 	id_calculadora INT NOT NULL,
     id_parametro INT NOT NULL,
-    CONSTRAINT FK_id_calculadora FOREIGN KEY (id_calculadora) REFERENCES calculadora(id),
-    CONSTRAINT FK_id_parametro FOREIGN KEY (id_parametro) REFERENCES parametro(id)
+    CONSTRAINT FK_id_parametros_calculadora FOREIGN KEY (id_calculadora) REFERENCES calculadora(id),
+    CONSTRAINT FK_id_parametros_parametro FOREIGN KEY (id_parametro) REFERENCES parametro(id)
 );
 
-CREATE TABLE IF NOT EXISTS parametro (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nombre VARCHAR(32) NOT NULL,
-    unidad_metrica VARCHAR(16) NOT NULL,
-    tipo_campo ENUM('text', 'number', 'select', 'radio') NOT NULL,
-    #valores FOREIGN KEY (null)
-)
+CREATE TABLE IF NOT EXISTS unidades_metricas (
+    id INT NOT NULL PRIMARY KEY,
+    nombre VARCHAR(32) NOT NULL,
+    abreviatura VARCHAR(16) NOT NULL,
+    id_parametro INT NOT NULL,
+    CONSTRAINT FK_id_unidades_metricas_parametro FOREIGN KEY (id_parametro) REFERENCES parametro(id)
+);
+
+CREATE TABLE IF NOT EXISTS valores (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_parametro INT NOT NULL,
+    valor VARCHAR(32) NOT NULL,
+    CONSTRAINT FK_id_valores_parametro FOREIGN KEY (id_parametro) REFERENCES parametro(id)
+);
+
+CREATE TABLE IF NOT EXISTS evidencia (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    enlace VARCHAR(500) NOT NULL,
+    id_calculadora INT NOT NULL,
+    CONSTRAINT FK_id_evidencia_calculadora FOREIGN KEY (id_calculadora) REFERENCES calculadora(id)
+);
