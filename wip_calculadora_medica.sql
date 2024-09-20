79d282b4f04271e046f59f63cbcf280860df1d4b
JuanAngulo <jua4_20@alu.uabcs.mx>
2024-09-20 15:23:04 -0600

    Update: Remove unused field in calculadora table

--- wip_calculadora_medica.sql
+++ wip_calculadora_medica.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS calculadora (
 	id INT NOT NULL PRIMARY KEY,
 	nombre VARCHAR(60) NOT NULL,
     descripción VARCHAR(360) NOT NULL,
-    descripcion_corta VARCHAR(120) NOT NULL,
+    descripcion_corta VARCHAR(120),
     instrucciones VARCHAR(360),
     formula VARCHAR(360) NOT NULL,
     fecha_creacion DATETIME NOT NULL,
@@ -17,7 +17,8 @@ CREATE TABLE IF NOT EXISTS calculadora (
 CREATE TABLE IF NOT EXISTS parametro (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nombre VARCHAR(32) NOT NULL,
-    tipo_campo ENUM('texto', 'numerico', 'seleccion', 'radio', 'check') NOT NULL
+    unidad_metrica VARCHAR(16),
+    tipo_campo ENUM('numerico', 'seleccion', 'radio') NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS parametros (
