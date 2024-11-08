1bc0ca9ddb93c1473f769baf359da3520d32f4c5
AnguloJuan <jua4_20@alu.uabcs.mx>
2024-11-08 06:53:06 -0700

    Actualizado middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,25 @@ import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 import { decrypt } from './utils/sessions';
 
+const protectedRoutes = ['/dashboard', 'nueva-calculadora', '/calculadoras', '/parametros', '/unidades']
+const publicRoutes = ['/iniciar-sesion', '/calculadoras:path*', '/']
+
 export default async function middleware(request: NextRequest) {
   const cookie = cookies().get('session')?.value;
   const session = await decrypt(cookie);
+  const path = request.nextUrl.pathname
+  const isProtectedRoute = protectedRoutes.includes(path)
+  const isPublicRoute = publicRoutes.includes(path)
 
-  if (request.nextUrl.pathname.startsWith('/nueva-calculadora') && !session) return NextResponse.redirect(new URL('/iniciar-sesion', request.url));
+  if (isProtectedRoute && !session) {
+    return NextResponse.redirect(new URL('/iniciar-sesion', request.nextUrl))
+  }
+
+  // if (request.nextUrl.pathname.startsWith('/nueva-calculadora') && !session) return NextResponse.redirect(new URL('/iniciar-sesion', request.url));
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    '/api/:path*',
-    '/nueva-calculadora',
-    '/dashboard',
-  ]
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
