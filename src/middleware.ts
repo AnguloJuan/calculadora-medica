afac0684c3b0493b3dad75a27a23451020f677a2
JuanAngulo <jua4_20@alu.uabcs.mx>
2024-11-14 16:16:07 -0700

    Arreglado middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,19 @@ import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 import { decrypt } from './utils/sessions';
 
-const protectedRoutes = ['/dashboard', 'nueva-calculadora', '/calculadoras', '/parametros', '/unidades']
-const publicRoutes = ['/iniciar-sesion', '/calculadoras:path*', '/']
+const protectedRoutes = ['/dashboard', '/nueva-calculadora', '/calculadoras', '/parametros', '/unidades'];
+const publicRoutes = ['/iniciar-sesion', '/calculadoras:path*', '/'];
 
 export default async function middleware(request: NextRequest) {
   const cookie = cookies().get('session')?.value;
   const session = await decrypt(cookie);
-  const path = request.nextUrl.pathname
-  const isProtectedRoute = protectedRoutes.includes(path)
-  const isPublicRoute = publicRoutes.includes(path)
+  const path = request.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.some(route => path.startsWith(route));
+  const isPublicRoute = publicRoutes.some(route => path.startsWith(route));
 
   if (isProtectedRoute && !session) {
-    return NextResponse.redirect(new URL('/iniciar-sesion', request.nextUrl))
+    return NextResponse.redirect(new URL('/iniciar-sesion', request.nextUrl));
   }
-
-  // if (request.nextUrl.pathname.startsWith('/nueva-calculadora') && !session) return NextResponse.redirect(new URL('/iniciar-sesion', request.url));
 
   return NextResponse.next();
 }
