52850bdff613b6d378c9240e0fd4db9d200822b7
JuanAngulo <jua4_20@alu.uabcs.mx>
2024-11-19 20:29:23 -0700

    Actualizado CrearParametro y FormularioParametro

--- src/components/CrearParametro.tsx
+++ src/components/CrearParametro.tsx
@@ -6,6 +6,7 @@ import {
   DialogDescription,
   DialogFooter,
   DialogHeader,
+  DialogPortal,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
@@ -15,57 +16,61 @@ import { useToast } from "@/zustand/Toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, useFormContext } from "react-hook-form";
 import FormularioParametro from "./formularios/FormularioParametro";
+import { useState } from "react";
 
-const CrearParametro = () => {
+const CrearParametro = ({element}: {element?: Element | DocumentFragment}) => {
   const { addToast } = useToast();
+  const [open, setOpen] = useState(false);
 
   const methods = useFormContext();
-  const form = useForm<z.infer<typeof ParametroSchema>>({
-    resolver: zodResolver(ParametroSchema),
+  const handleValidaton = () => {
+    return zodResolver(ParametroSchema);
+  };
+  const formParametro = useForm<z.infer<typeof ParametroSchema>>({
+    resolver: handleValidaton(),
     defaultValues: {
       id: 0,
-      nombre: undefined,
-      abreviatura: undefined,
-      tipo_campo: undefined,
-      valorMinimo: undefined,
-      valorMaximo: undefined,
-      unidades: [],
-      opciones: undefined,
     },
   })
-  if (methods) {
-    console.log(methods);
-  } else {
-    console.log('no methods', form);
-  }
+  // if (methods) {
+  //   console.log(methods);
+  // } else {
+  //   console.log('no methods', form);
+  // }
 
-  const onSubmit = (data: any) => {
+  const submitParametro = (data: any) => {
+    const output = formParametro.trigger()
+console.log(output);
+
+    if (!output) {
+      return;
+    }
     console.log(data);
     addToast('Parametro creado', 'success');
+    setOpen(false);
   }
 
   return (<>
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="default">Crear parametro</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-lg lg:max-w-lg">
-        <DialogHeader>
-          <DialogTitle>Crear parametro</DialogTitle>
-          <DialogDescription>
-            {!methods ?
-              "Al crear un parametro, este se agregará a la lista de parametros disponibles para las calculadoras."
-              : "Este parametro se agregará a la lista de parametros de la calculadora."
-            }
-          </DialogDescription>
-        </DialogHeader>
-        <section className="grid gap-4 py-4">
-          <FormularioParametro form={form} onSubmit={onSubmit} />
-        </section>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
+    <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="default">Crear parametro</Button>
+        </DialogTrigger>
+      <DialogPortal container={element}>
+        <DialogContent className="sm:max-w-lg lg:max-w-lg">
+          <DialogHeader>
+            <DialogTitle>Crear parametro</DialogTitle>
+            <DialogDescription>
+              {/* {!methods ?
+                "Al crear un parametro, este se agregará a la lista de parametros disponibles para las calculadoras."
+                : "Este parametro se agregará a la lista de parametros de la calculadora."
+              } */}
+            </DialogDescription>
+          </DialogHeader>
+          <section className="grid gap-4 py-4 max-w-full">
+            <FormularioParametro formParametro={formParametro} submitParametro={submitParametro} />
+          </section>
+        </DialogContent>
+      </DialogPortal>
     </Dialog>
   </>)
 }
--- src/components/formularios/FormularioParametro.tsx
+++ src/components/formularios/FormularioParametro.tsx
@@ -1,25 +1,124 @@
-import { Control, FormProps, FormState, UseFormReturn } from "react-hook-form"
-import { Form } from "../ui/form"
+import { Control, FormProps, FormState, useController, UseFormReturn } from "react-hook-form"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
 import { ParametroSchema } from "@/validationSchemas/ParametroSchema"
 import { z } from "@/lib/es-zod"
 import FormInput from "../ui/form-input"
+import Select from "react-select";
+import { useEffect, useState } from "react"
+import { Unidad } from "@/utils/types"
+import { Select as MSelect, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
+import { DialogClose, DialogFooter } from "../ui/dialog"
+import { Button } from "../ui/button"
 
 type ParametroFields = z.infer<typeof ParametroSchema>
 
-const FormularioParametro = ({ form, onSubmit }: {
-  form: UseFormReturn<ParametroFields>,
-  onSubmit: (data: ParametroFields) => void
+const FormularioParametro = ({ formParametro, submitParametro }: {
+  formParametro: UseFormReturn<ParametroFields>,
+  submitParametro: (data: ParametroFields) => void
 }) => {
+  const { field: tipoCampo } = useController({ name: 'tipo_campo', 'control': formParametro.control })
+  const [Unidades, setUnidades] = useState<{ value: Unidad, label: string }[]>([])
+
+  useEffect(() => {
+    const obtenerUnidades = async () => {
+      const response = await fetch('/api/unidades')
+      const data = await response.json()
+
+      setUnidades(data.unidades.map((unidad: Unidad) => ({ value: unidad, label: unidad.unidad })))
+    }
+    obtenerUnidades()
+  }, [])
 
   return (<>
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+    <Form {...formParametro}>
+      <form>
         <FormInput
-          control={form.control}
+          control={formParametro.control}
           name="nombre"
           label="Parametro"
           placeholder="Nombre del parametro"
         />
+        <FormField
+          control={formParametro.control}
+          name="tipo_campo"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Tipo de campo</FormLabel>
+              <MSelect onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Seleccióna el tipo de campo" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="numerico">Numérico</SelectItem>
+                  <SelectItem value="seleccion">Selección</SelectItem>
+                  <SelectItem value="radio">Radio</SelectItem>
+                </SelectContent>
+              </MSelect>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {tipoCampo.value === 'numerico' && (<>
+          <div className="flex flex-row gap-2">
+            <FormField
+              control={formParametro.control}
+              name='unidades'
+              render={({ field }) => (<>
+                <FormItem>
+                  <FormLabel>Parametros</FormLabel>
+                  <div className="flex flex-row gap-2 w-full">
+                    <FormControl>
+                      <Select
+                        {...field}
+                        options={Unidades}
+                        noOptionsMessage={() => 'No hay unidades disponibles'}
+                        isMulti
+                        onChange={(value: any) => formParametro.setValue('unidades', value)}
+                        className="w-full"
+                      />
+                    </FormControl>
+                  </div>
+                  <FormDescription>
+                    El area a la que pertenece la calculadora
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              </>
+              )}
+            />
+          </div>
+          <div className="flex flex-row gap-4">
+            <FormInput
+              control={formParametro.control}
+              name="valorMinimo"
+              label="Valor mínimo"
+              placeholder="Valor mínimo"
+            />
+            <FormInput
+              control={formParametro.control}
+              name="valorMaximo"
+              label="Valor máximo"
+              placeholder="Valor máximo"
+            />
+          </div>
+        </>)}
+
+        {(tipoCampo.value === 'seleccion' || tipoCampo.value === 'radio') && (<>
+          <FormInput
+            control={formParametro.control}
+            name="opciones"
+            label="Opciones"
+            placeholder="Opciones separadas por coma"
+          />
+        </>)}
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="secondary">Cancel</Button>
+          </DialogClose>
+          <Button type="button" onClick={()=> submitParametro(formParametro.getValues())}>Save changes</Button>
+        </DialogFooter>
       </form>
     </Form>
   </>)
