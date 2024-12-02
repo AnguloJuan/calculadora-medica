3e89c3cad8aafaf763b0d3aad86225e47f2fd08c
AnguloJuan <jua4_20@alu.uabcs.mx>
2024-12-01 17:07:56 -0700

    Fixed calculadoras fetch and asingment of parametros in /calculadoras

--- src/app/(admin)/calculadoras/page.tsx
+++ src/app/(admin)/calculadoras/page.tsx
@@ -2,39 +2,29 @@ import { CalculadoraTable, columns } from "@/components/calculadoras/columns";
 import CrearCalculadora from "@/components/calculadoras/CrearCalculadora";
 import { DataTable } from "@/components/data-table";
 import { conectarBd } from "@/db/conectarDb";
-import { Calculadora, Parametro, Unidad } from "@/utils/types";
+import { Calculadora, Evidencia, Parametro, Unidad } from "@/utils/types";
 import { TypeCalculadoraSchema } from "@/validationSchemas/CalculadoraSchema";
 import { TypeParametroSchema } from "@/validationSchemas/ParametroSchema";
 import { RowDataPacket } from "mysql2";
 
 const CalculadorasPage = async () => {
   const conexion = await conectarBd()
-  const obtenerCalculadoras = async () => {
-    interface Calculadoras extends RowDataPacket, Calculadora { }
-    try {
-      const [calculadorasRows] = await conexion.query<Calculadoras[]>(
-        'SELECT * FROM `calculadora`',
-      );
 
-      return calculadorasRows;
-    } catch (error) {
-      console.error(error);
-    }
-  }
   const obtenerParametros = async () => {
     interface Parametros extends RowDataPacket, Parametro { }
-    try {
-      const [parametrosRows] = await conexion.query<Parametros[]>(
-        'SELECT * FROM `parametro`',
-      );
-
-      return parametrosRows;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  const obtenerUnidades = async (parametros: Parametro[]) => {
     interface Unidades extends RowDataPacket, Unidad { }
+    async function obtenerParams() {
+      try {
+        const [parametros] = await conexion.query<Parametros[]>(
+          'SELECT * FROM `parametro`',
+        );
+        return parametros;
+      } catch (error) {
+        console.error(error);
+        return [];
+      }
+    }
+    const parametros = await obtenerParams();
     const parametrosConUnidades = parametros.map(async (parametro) => {
       try {
         const [unidadesRows] = await conexion.query<Unidades[]>(
@@ -52,32 +42,52 @@ const CalculadorasPage = async () => {
     return resolvedParametros.filter((parametro) => parametro !== undefined) as TypeParametroSchema[];
   }
 
-  const calculadoras = await obtenerCalculadoras();
-  const parametrosObtenidos = await obtenerParametros();
-  const parametros: TypeParametroSchema[] = await obtenerUnidades(parametrosObtenidos!);
-  const calculadorasConParametros: TypeCalculadoraSchema[] = calculadoras ? calculadoras.map((calculadora) => {
-    return {
-      id: calculadora.id,
-      nombre: calculadora.nombre,
-      descripcion: calculadora.descripcion,
-      categoria: calculadora.categoria,
-      formula: calculadora.formula,
-      parametros: parametros.filter((parametro) => parametro.id === calculadora.id) as [TypeParametroSchema, ...TypeParametroSchema[]],
-      evidencias: calculadora.evidencias,
-      descripcion_corta: calculadora.descripcion_corta,
-      resultados_recomendaciones: calculadora.resultados_recomendaciones,
-      enlace: calculadora.enlace,
-    }
-  }) : []
+  const parametros: TypeParametroSchema[] = await obtenerParametros();
 
-  const data: CalculadoraTable[] = calculadorasConParametros ? calculadorasConParametros.map((calculadora) => {
+  // Obtener calculadoras con evidencias y parametros
+  async function obtenerCalculadoras() {
+    interface RowsCalculadora extends RowDataPacket, Calculadora { }
+    interface Evidencias extends RowDataPacket, Evidencia { }
+
+    try {
+      const [calcRows] = await conexion.query<RowsCalculadora[]>('SELECT * FROM calculadora');
+      const [evidencieaRows] = await conexion.query<Evidencias[]>('SELECT * FROM evidencia');
+      const [relations] = await conexion.query<RowDataPacket[]>('SELECT * FROM `calculadora_parametro`');
+      
+      const calculadorasConParametros = calcRows.map((calculadora) => {
+        return {
+          ...calculadora,
+          parametros: parametros.filter((parametro) => relations.some((relation) => relation.id_calculadora === calculadora.id && relation.id_parametro === parametro.id)) as any,
+        }
+      });
+
+      const calculadoras = calculadorasConParametros.map((calc) => {
+        return {
+          ...calc,
+          evidencias: (evidencieaRows.filter((evidencia) => evidencia.id_calculadora === calc.id).map((evidencia) => ({
+            enlace: evidencia.enlace,
+            cita: evidencia.cita,
+          })) as [{ enlace: string; cita: string; }, ...{ enlace: string; cita: string; }[]])
+        }
+      });
+
+      return calculadoras
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  const calculadoras: TypeCalculadoraSchema[] = await obtenerCalculadoras();
+
+  const data: CalculadoraTable[] = calculadoras ? calculadoras.map((calculadora) => {
     return {
       id: calculadora.id,
       nombre: calculadora.nombre,
       categoria: calculadora.categoria,
       formula: calculadora.formula,
       calculadora: calculadora,
-      parametros: parametros,
+      parametros: calculadora.parametros.length > 0 ? calculadora.parametros : [] as any,
     }
   }) : []
 
